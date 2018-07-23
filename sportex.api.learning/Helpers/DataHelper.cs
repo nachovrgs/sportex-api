10073f639b3d281fec0d194a6b20a4e82261925e
nachovrgs <ignaciovrgs@hotmail.com>
2018-07-22 21:01:51 -0300

    add helper for ML

---
+++ sportex.api.learning/Helpers/DataHelper.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sportex.api.learning.Helpers
+{
+    public static class DataHelper
+    {
+
+        public static double[][] LoadData(string filePath)
+        {
+            TextReader readFile = new StreamReader(filePath);
+            var csv = new CsvReader(readFile);
+            var records = csv.GetRecords<Item>();
+            double[][] data = new double[1000][];
+
+            int i = 0;
+            foreach (var row in records)
+            {
+                data[i] = new double[] { row.distance, row.age, row.result };
+                i++;
+            }
+            return data;
+        }
+    }
+}
---
+++ sportex.api.learning/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sportex.api.learning
+{
+    public class Item
+    {
+        public double distance { get; set; }
+        public double age { get; set; }
+        public double result { get; set; }
+    }
+}
